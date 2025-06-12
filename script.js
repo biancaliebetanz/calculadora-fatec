712568e4407a6136a32f9302df533098d9dc94d9
isaoficial18 <isaoficial18@gmail.com>
2025-06-12 13:19:53 +0000

    fix: scrip de notas

--- script.js
+++ script.js
@@ -62,27 +62,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-function calcularMedia(){
-    var tn1= window.document.getElementById('n1')
-    var tn2= window.document.getElementById('n2')
-    var res = window.document.getElementById('res')
-    var n1= Number(tn1.value)
-    var n2= Number(tn2.value)
-    var media= (n1+n2)/2
-    res.innerHTML= media
-}
-var botaoLimparNota = document.getElementById('botaoLimparNota');
-var n1Input = document.getElementById('n1');
-var n2Input = document.getElementById('n2');
-var res = document.getElementById('res');
-
-botaoLimparNota.addEventListener("click", function () {
-    n1Input.value = "";
-    n2Input.value = "";
-    res.textContent = "...";
-});
-
-
 
 function calcularPorcentagem(){
     var tn1 = document.getElementById('porcentagem') 
